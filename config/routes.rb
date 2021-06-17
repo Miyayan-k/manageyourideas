4ca8309e51d55597dff0bf62a8370d38d0dd9d21
Miyayan-k <kentaro.miyayan2077hornet@gmail.com>
2021-06-17 22:44:25 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # 今後のAPIバージョン管理を想定
   namespace 'api' do
     namespace 'ver1' do
-      resources :ideas
+      resources :ideas, only: [:index, :create]
     end
   end
 end
