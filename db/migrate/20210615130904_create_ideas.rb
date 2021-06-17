fd4140785a343bd27b1c2c1bf9d14e99f5c563a9
Miyayan-k <kentaro.miyayan2077hornet@gmail.com>
2021-06-17 22:41:50 +0900

    fine tuning on ideas_migration_file

--- db/migrate/20210615130904_create_ideas.rb
+++ db/migrate/20210615130904_create_ideas.rb
@@ -1,7 +1,7 @@
 class CreateIdeas < ActiveRecord::Migration[6.0]
   def change
     create_table :ideas do |t|
-      t.text       :body,     null:false
+      t.text       :body,     null: false
       t.references :category, null: false, foreign_key: true
       t.timestamps
     end
