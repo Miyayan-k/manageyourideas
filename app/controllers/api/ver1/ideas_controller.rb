4388980a449e04a8a6fd6403022a879064d2fece
Miyayan-k <kentaro.miyayan2077hornet@gmail.com>
2021-06-19 16:51:13 +0900

    Update ideas_controller.rb

--- app/controllers/api/ver1/ideas_controller.rb
+++ app/controllers/api/ver1/ideas_controller.rb
@@ -28,9 +28,9 @@ module Api
         @idea_category = IdeaCategory.new(idea_params)
         if @idea_category.valid?
           @idea_category.save
-          render json: { status: 201, message: 'アイデアの登録に成功しました。', data: @idea_category }
+          render status: 201, json: { status: 201, message: 'アイデアの登録に成功しました。', data: @idea_category }
         else
-          render json: { status: 422, message: 'アイデアの登録に失敗しました。', data: @idea_category.errors.full_messages }
+          render status: 422, json: { status: 422, message: 'アイデアの登録に失敗しました。', data: @idea_category.errors.full_messages }
         end
       end
 
